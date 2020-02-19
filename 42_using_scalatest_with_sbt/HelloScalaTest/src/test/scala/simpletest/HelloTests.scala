ec6511780bbb13312d6b0095afc6f1317db2f8a4
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-02-18 17:28:57 -0800

    scalatest and sbt

---
+++ 42_using_scalatest_with_sbt/HelloScalaTest/src/test/scala/simpletest/HelloTests.scala
@@ -0,0 +1,16 @@
+package simpletest
+
+import org.scalatest.FunSuite
+
+class HelloTests extends FunSuite {
+  test("the name is set correctly in constructor") {
+    val p = new Person("Barney Rubble")
+    assert(p.name == "Barney Rubble")
+  }
+
+  test("A Persons name can be changed") {
+    val p = new Person("Chad Johnson")
+    p.name = "Ochocinco"
+    assert(p.name == "Ochocinco")
+  }
+}
